83f2e2939713bc6b6b8bfe2fbea731bc8ae5af58
Kenneth <kenneth.reilly@hotmail.com>
2014-11-16 08:49:47 -0600

    Added authentication

--- MathCollab/app/routes.js
+++ MathCollab/app/routes.js
@@ -36,25 +36,127 @@
         failureFlash: true // allow flash messages
     }));
 
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+		    successRedirect : '/pages/profile',
+		    failureRedirect : '/'
+		}));
+
+   
+    // =====================================
+    // TWITTER ROUTES ======================
+    // =====================================
+    // route for twitter authentication and login
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', {
+		    successRedirect: '/pages/profile',
+		    failureRedirect: '/'
+		}));
+
+
 
     // =====================================
     // PROFILE SECTION =====================
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    //app.get('/profile', isLoggedIn, function (req, res) {
-    //    res.render('profile.ejs', {
-    //        user: req.user // get the user out of session and pass to template
-    //    });
-    //});
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('pages/profile.ejs', {
+            user: req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =============================================================================
+    // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
+    // =============================================================================
+
+    // locally --------------------------------
+    app.get('/connect/local', function (req, res) {
+        res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+    });
+    app.post('/connect/local', passport.authenticate('local-signup', {
+        successRedirect: '/pages/profile', // redirect to the secure profile section
+        failureRedirect: '/connect/local', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+    // facebook -------------------------------
+
+    // send to facebook to do the authentication
+    app.get('/connect/facebook', passport.authorize('facebook', { scope: 'email' }));
+
+    // handle the callback after facebook has authorized the user
+    app.get('/connect/facebook/callback',
+        passport.authorize('facebook', {
+            successRedirect: '/pages/profile',
+            failureRedirect: '/'
+        }));
+
+    // twitter --------------------------------
+
+    // send to twitter to do the authentication
+    app.get('/connect/twitter', passport.authorize('twitter', { scope: 'email' }));
+
+    // handle the callback after twitter has authorized the user
+    app.get('/connect/twitter/callback',
+        passport.authorize('twitter', {
+            successRedirect: '/pages/profile',
+            failureRedirect: '/'
+        }));
+
+    // =============================================================================
+    // UNLINK ACCOUNTS =============================================================
+    // =============================================================================
+    // used to unlink accounts. for social accounts, just remove the token
+    // for local account, remove email and password
+    // user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', function (req, res) {
+        var user = req.user;
+        user.local.email = undefined;
+        user.local.password = undefined;
+        user.save(function (err) {
+            res.redirect('/pages/profile');
+        });
+    });
+
+    // facebook -------------------------------
+    app.get('/unlink/facebook', function (req, res) {
+        var user = req.user;
+        user.facebook.token = undefined;
+        user.save(function (err) {
+            res.redirect('/pages/profile');
+        });
+    });
+
+    // twitter --------------------------------
+    app.get('/unlink/twitter', function (req, res) {
+        var user = req.user;
+        user.twitter.token = undefined;
+        user.save(function (err) {
+            res.redirect('/pages/profile');
+        });
+    });
+
 
     // =====================================
     // LOGOUT ==============================
     // =====================================
-    //app.get('/logout', function (req, res) {
-    //    req.logout();
-    //    res.redirect('/');
-    //});
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 };
 
     // route middleware to make sure a user is logged in
---
+++ MathCollab/config/auth.js
@@ -0,0 +1,23 @@
+﻿    // config/auth.js
+
+    // expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth': {
+        'clientID': '294500654073624', // your App ID
+        'clientSecret': '4d20808b85f5205a747d94671c7fa211', // your App Secret
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    },
+    
+	'twitterAuth' : {
+		'consumerKey' 		: 'pFW70SMQuLQRJwi4QeWBIh0dR',
+		'consumerSecret' 	: 'BlEc7ymcJF6Z59A4bZZH4NJBq8ScaoT4RpuGZH0TBYVbRd2J0f',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	}
+};
